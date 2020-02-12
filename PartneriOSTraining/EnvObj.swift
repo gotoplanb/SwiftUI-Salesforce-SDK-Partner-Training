e52532c7ac27fd2259969dee40ce1e33ca2e1ca3
Kevin Poorman <kjp@codefriar.com>
2020-02-11 23:42:15 -0500

    SF DX Org

---
+++ PartneriOSTraining/EnvObj.swift
@@ -0,0 +1,9 @@
+//
+//  EnvObj.swift
+//  PartneriOSTraining
+//
+//  Created by Kevin Poorman on 2/11/20.
+//  Copyright © 2020 PartneriOSTrainingOrganizationName. All rights reserved.
+//
+
+import Foundation
